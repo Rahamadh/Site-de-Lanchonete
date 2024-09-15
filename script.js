8cf5e80392bd9f003cb2de1d648706953097dde5
Felipe <rahamad.mustapha@gmail.com>
2024-09-14 21:42:28 -0300

    atualização de layout

---
+++ script.js
@@ -0,0 +1,13 @@
+/*let contador = 0; // Initialize contador
+
+const botao = document.getElementById('addcar');
+const carrinho = document.getElementById('carrinho');
+const quantidade = document.getElementById('quantidade');
+
+botao.addEventListener('click', () => {
+  carrinho.style.display = 'block';
+  contador++;
+  quantidade.textContent = contador;
+});
+
+*/
